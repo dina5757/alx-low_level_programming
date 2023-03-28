275baac0331c3b8a470322872c52b99c6655aa71
dina <dina39502@gimal.com>
2023-03-27 18:13:23 -0700

    my

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -3,8 +3,9 @@
 /**
  * puts2 - function should print only one character out of two
  * starting with the first one
- * @str: print
- */
+ * @str: input
+ * Return: print
+*/
 void puts2(char *str)
 {
 	int longi = 0;
@@ -18,7 +19,7 @@ void puts2(char *str)
 		longi++;
 	}
 	t = longi - 1;
-	for (o = 0 ; o <= t ; o++)
+	for (o = 0; o <= t ; o++)
 	{
 		if (o % 2 == 0)
 	}
@@ -26,5 +27,5 @@ void puts2(char *str)
 		_putchar(str[o]);
 	}
 	}
-		_putchar('\n');
+	_putchar('\n');
 }
